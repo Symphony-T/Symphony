463c527965b1a540978162f3967ddfdc9c62e6e6
omarzouaoui <zouaouiomar80@gmail.com>
2022-07-20 16:56:36 +0100

    x

---
+++ client/components/Conservatory.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+<h1>Conservatory</h1>
+<form>
+  <div>
+    <input type="text" placeholder="Name" name="name" id="name" required>
+
+    <input type="text" placeholder="Description" name="description" id="description" required>
+
+    <input type="text" placeholder="Img link" name="img" id="img" required>
+
+     <input type="text" placeholder="Localisation" name="localisation" id="localisation" required>
+
+    <input type="number" placeholder="Price per month" name="price" id="price" required>
+    <br><br>
+    <input type="number" placeholder="Phne Number" name="PhoneN" id="PhoneN" required>
+    <br><br>
+    <button type="submit">Add one</button>
+  </div>
+  </form>
+</div>
+  </template>
+
+<style>
+h1{
+    text-align: center;
+}
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,11 +1,14 @@
 <template>
-  <Tutorial />
+  <Conservatory/>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Conservatory from '~/components/Conservatory.vue'
+
 
 export default Vue.extend({
-  name: 'IndexPage'
+    name: "IndexPage",
+    components: { Conservatory}
 })
 </script>
