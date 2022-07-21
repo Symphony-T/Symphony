32f45a77f42cc913962ddcd98928ba2f84593971
Fawez11 <fawezferjaoui11@gmail.com>
2022-07-21 09:22:10 +0200

    routes

---
+++ client/pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>About</h1>
+        <NuxtLink to ="/">Home</NuxtLink>
+    </div>
+
+</template>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,11 +1,11 @@
 <template>
 
-  <!-- <SignUp /> -->
+  <nuxt-link to="/about">about</nuxt-link> 
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import SignUpVue from '~/components/SignUp.vue'
+import SignUpVue from '../components/SignUp.vue'
 
 export default Vue.extend({
   name: 'IndexPage'
