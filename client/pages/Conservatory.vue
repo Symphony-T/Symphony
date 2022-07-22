7017d8e84c27d1516843a1c227d762fe6ece6e1c
omarzouaoui <zouaouiomar80@gmail.com>
2022-07-22 16:47:17 +0100

    x

--- client/pages/Conservatory.vue
+++ client/pages/Conservatory.vue
@@ -1,34 +1,34 @@
 <template>
 <div>
   <div>
-    <Navbar />
+    <!-- <Navbar /> -->
   </div>
 
 <h1>Conservatory</h1>
   <form  style="max-width:500px;margin:auto">
   <h2>Create a new one</h2>
   <div class="input-container">
-    <input class="input-field" type="text" v-model="name" placeholder="Name" name="name">
+    <input class="i" type="text" v-model="name" placeholder="Name" name="name">
   </div>
-    <input class="input-field" type="text" v-model="description" placeholder="Description" name="description">  
-  <div class="input-container"><br> 
-   <input class="input-field" type="text" v-model="img" placeholder="Img" name="img">
+    <input class="i" type="text" v-model="description" placeholder="Description" name="description">  
+  <div class="input-container">
+   <input class="i" type="text" v-model="img" placeholder="Img" name="img" id="xxx">
   </div>
   <div class="input-container">
-    <input class="input-field" type="text" v-model="localisation" placeholder="Localisation" name="localisation">
+    <input class="i" type="text" v-model="localisation" placeholder="Localisation" name="localisation">
   </div>
   <div class="input-container">
-    <input class="input-field" type="number" v-model="price" placeholder="Price" name="price">
+    <input class="i" type="number" v-model="price" placeholder="Price" name="price">
   </div>
   <div class="input-container">
-    <input class="input-field" type="number" v-model="phoneN" placeholder="Phone Number" name="phoneN">
+    <input class="i" type="number" v-model="phoneN" placeholder="Phone Number" name="phoneN">
   </div>
 
-  <button v-on:click="saveCons" class="btn">Post new one 🎹</button><br><br>
+  <NuxtLink to="allcons"  v-on:click="saveCons" id="b">Post new one 🎹</NuxtLink><br><br>
   <NuxtLink to="allcons" class="cancel">Cancel</NuxtLink>
 
 </form>  
-<NuxtLink to="allcons" class="bttn">See all Conservatorys </NuxtLink>
+<!-- <NuxtLink to="allcons" class="bttn">See all Conservatorys </NuxtLink> -->
 </div>
   </template>
 
@@ -80,6 +80,60 @@ export default defineComponent ({
 
 </script>
 <style>
+#b{
+                display: inline-block;
+                outline: 0;
+                border: 0;
+                cursor: pointer;
+                background: #000000;
+                color: #FFFFFF;
+                border-radius: 8px;
+                padding: 14px 24px 16px;
+                font-size: 18px;
+                font-weight: 700;
+                line-height: 1;
+                transition: transform 200ms,background 200ms;
+               text-decoration: none; 
+               margin-left: 145px;
+               }
+#b:hover{
+                    transform: translateY(-2px);
+                }
+#xxx{
+  margin-top: 15px;
+}
+.i{
+                    padding: 0 10px;
+                    text-align: left;
+                    transition-property: box-shadow, background;
+                    transition-duration: 150ms;
+                    transition-timing-function: ease-in-out;
+                    background-color: #FFFFFF;
+                    border-width: 2px;
+                    border-style: solid;
+                    border-color: #DAE3F3;
+                    border-radius: 0px;
+                    color: #272C34;
+                    width: 100%;
+                    cursor: text;
+                    font-size: 14px;
+                    line-height: 20px;
+                    letter-spacing: 0px;
+                    font-weight: 400;
+                    font-style: normal;
+                    height: 40px;
+                   
+                                
+}
+.i:hover{
+                        box-shadow: 0px 2px 4px rgb(39 44 52 / 12%);
+                    }
+                    :focus{
+                        transition-property: border-color, box-shadow, background;
+                        border-color: #39414D;
+                        outline: 0px;
+                        box-shadow: 0 0 0 3px #c5d2e7;
+                    }
 h2{
   text-align: center;
   color: rgb(21, 111, 185);
@@ -87,48 +141,29 @@ h2{
 h1{
   text-align: center;
   float: center;
-}
- .bttn{
-    position: relative;
-    margin:auto;
-    width: 600px;
-    height: 45px;
-    font-size: 18px;
-    text-transform: uppercase;
-    letter-spacing: 2.5px;
-    font-weight: 500;
-    color: white;
-    background-color: rgb(6, 1, 6);
-    border: none;
-    border-radius: 45px;
-    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
-    transition: all 0.3s ease 0s;
-    cursor: pointer;
-    outline: none;
-   text-decoration: none;
-    font-family: 'Times New Roman', Times, serif;
+} 
+.cancel{
+ 
+                display: inline-block;
+                outline: 0;
+                border: 0;
+                cursor: pointer;
+                border-radius: 8px;
+                padding: 14px 24px 16px;
+                font-size: 18px;
+                font-weight: 700;
+                line-height: 1;
+                transition: transform 200ms,background 200ms;
+                background: transparent;
+                color: #000000;
+                box-shadow: 0 0 0 3px #000000 inset;
+                text-decoration: none; 
+                margin-left: 180px;             
 }
 
-.bttn:hover {
-    background-color: orange;
-}
-
-.cancel {
-  background-color: rgb(102, 102, 102);
-  color: white;
-  padding: 15px 20px;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  opacity: 0.9;
-  text-decoration: none;
-  margin: auto;
-  float: center;
-}
-
-.cancel:hover {
-  opacity: 1;
-}
+.cancel:hover{
+                    transform: translateY(-2px);
+                }
 </style>
 
 
--- client/pages/allcons.vue
+++ client/pages/allcons.vue
@@ -2,22 +2,26 @@
 <div>
     <div>
 <Navbar /> 
-  </div>
-        <div class="hold">
-            <NuxtLink to="conservatory" class="btn--1">Create One </NuxtLink>
-            <div class="container" v-for="conser in c" :key="conser.id">
-                <div class="CContainer">
-                        <h4>{{conser.name}}</h4>
-                        <p>{{conser.description}}</p>
-                        <img :src="`${conser.img}`" alt="conservatory image">
-                        <h5>📍{{conser.localisation}}</h5>
-                        <h5>{{conser.price}}$</h5>
+  </div>                        
+        <div class="h">
+            <div id="cons" v-for="conser in c" :key="conser.id">
+                <div id= "cadre">
+                    <h4>{{conser.name}}</h4>
+                    <img :src="`${conser.img}`" alt="conservatory image" id="image">
+                        <h5>{{conser.description}}</h5>
+                        <h5>📍Localisation: {{conser.localisation}}</h5>
+                        <h5 id="phone">price :{{conser.price}}$</h5>
                         <h5>📞{{conser.phoneN}}</h5>
-                          <button v-on:click="" class="delete">Delete</button>
+                          <!-- <button v-on:click="" class="delete">Delete</button>
                           <button v-on:click="" class="update">Update</button>
+                          <star-rating></star-rating> -->
+                          <image-rating src="/images/vueLogo.png"></image-rating>
+                          <!-- <p on:click="rate">👍</p>  -->
+                          <!-- <p>Rate:{{counter}}</p> -->
                     </div>
             </div>
         </div>
+                    <NuxtLink to="conservatory" id="add">Create One </NuxtLink>
     </div>
 </template>
 
@@ -31,10 +35,17 @@ export default defineComponent({
     components: { Navbar },
     data() {
         return {
-            c: []
+            c: [],
+            counter:1
         }
-    },
-
+    }, methods: {
+    // showAlert: () => {
+    //   alert('Like')
+    // }
+    rate:()=>{
+        this.counter++
+    }
+  },
     created() {
         ConsService.getConservatory()
             .then(response => {
@@ -45,62 +56,70 @@ export default defineComponent({
 })
 </script>
 <style>
+#phone{
+    color: #000000;
+}
+.h{
+    margin-top: 50px;
+}
 h4{
-    color: rgb(78, 147, 238);
+    color: rgb(255, 0, 0);
 }
- .btn--1{
+#image{
     position: relative;
-    margin:auto;
-    width: 600px;
-    height: 45px;
-    font-size: 18px;
-    text-transform: uppercase;
-    letter-spacing: 2.5px;
-    font-weight: 500;
-    color: white;
-    background-color: rgb(6, 1, 6);
-    border: none;
-    border-radius: 45px;
-    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
-    transition: all 0.3s ease 0s;
-    cursor: pointer;
-    outline: none;
-   text-decoration: none;
-    font-family: 'Times New Roman', Times, serif;
+    width: 330px;
+    height: 200px;
+    border-radius:0 0 0 0;
 }
-
-.btn--1:hover {
-    background-color: orange;
-}
-img {
+#cadre {
     position: relative;
+    background-color: #ffffff;
+    height: 450px;
     width: 400px;
-    height: 400px;
-    border-top-left-radius: 80px;
-    border-top-right-radius: 80px;
-}
-.CContainer {
-    position: relative;
-    background-color: #f5f5f4;
-    width: 400px;
-    margin-top: 0;
+    margin-top: 20px;
     text-align: center;
     padding-bottom: 20px;
-    border-radius: 80px;
+    border-radius: 10px 10px;
+    background-color: #f5ecff;
 }
-.container {
+#cons{
+    width: 400px;
+    height: 450px;
     position: relative;
     float: left;
     margin-right: 50px;
     margin-left: 90px;
-    box-shadow: 0 11px 15px -7px rgb(0 0 0 / 20%), 0 24px 38px 3px rgb(0 0 0 / 14%), 0 9px 46px 8px rgb(0 0 0 / 12%);
-    margin-bottom: 30px;
-    border-radius: 80px;
-
+    /* box-shadow: 0 11px 15px -7px rgb(0 0 0 / 20%), 0 24px 38px 3px rgb(0 0 0 / 14%), 0 9px 46px 8px rgb(0 0 0 / 12%); */
+    margin-bottom: 50px;
+    margin-top: 50px;
+    border-radius: 10px 10px;
+    background-color: #f5ecff;
+    }
+#cons:hover {
+    position: relative;
+    /* box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset; */
+    cursor: pointer;
+}
+#add{
+                display: inline-block;
+                outline: 0;
+                border: 0;
+                cursor: pointer;
+                border-radius: 8px;
+                padding: 14px 24px 16px;
+                font-size: 18px;
+                font-weight: 700;
+                line-height: 1;
+                transition: transform 200ms,background 200ms;
+                background: transparent;
+                color: #000000;
+                box-shadow: 0 0 0 3px #000000 inset;
+                text-decoration: none; 
+                margin-left: 490px;             
 }
 
-.container:hover {
-    position: relative;
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
-    cursor: pointer;}
+#add:hover{
+                    transform: translateY(-2px);
+                }
+
 </style>
